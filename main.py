a90a92ad80ad70ad36219d887af896cc5fc688ab
Sameer <kalantrilonda@gmail.com>
2024-03-27 23:35:14 +0530

    add new command and inserted 2 new handlers

--- main.py
+++ main.py
@@ -20,11 +20,21 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+async def caps(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    capital_message = " ".join(context.args).upper()
+    await context.bot.sendMessage(chat_id=update.effective_chat.id, text=capital_message)
+
+
+    
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(token).build()
     
     start_handler = CommandHandler('start', start)
     message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler('caps', caps)
     application.add_handler(start_handler)
+    application.add_handler(message_handler)
+    application.add_handler(caps_handler)
     
     application.run_polling()
